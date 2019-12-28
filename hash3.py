58481b1c5efadec7fe5973ff3e9ef723d246b063
graficks <43565446+graficks@users.noreply.github.com>
2019-12-28 11:10:01 -0700

    Add files via upload

--- hash3.py
+++ hash3.py
@@ -2,8 +2,10 @@ import hashlib
 import binascii
 import base58
 import hmac
-#Perform SHA on binary
 
+N=0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
+
+#Perform SHA on binary
 binary_seed = "01011100010 11101100111 00010100110 10000001010 01110101111 10100001101 11100100000 00110000110 10110000001 10010000101 01001100001 0000100"
 
 binary_seed = binary_seed.replace(" ","")
@@ -38,7 +40,6 @@ for x in range(int(len(seed)/11)):
     word_index = int(seed[x*11 : x*11 + 11], 2)
     mnemonic += lines[word_index] + " "
 
-
 mnemonic = mnemonic.replace('\n','')
 #remove the last space
 mnemonic = mnemonic[:-1]
@@ -53,15 +54,20 @@ print(bip39_seed.hex())
 #bytes(mnemonic, 'utf-8')
 #mnemonic.encode('utf-8')
 
-
-
-print("bip32_ext_key: ") #or bip32 root key
+#bip32
 bip32_hash = hmac.new(b"Bitcoin seed", bip39_seed, digestmod=hashlib.sha512).digest()
+#depth = 0, master node
+master_chain_code = bip32_hash[32:]
+master_key = bip32_hash[:32]
+print("master_chain_code: ")
+print(master_chain_code.hex())
+print("master_key: ")
+print(master_key.hex())
 #(serialization format bip32) version, depth, parent's fingerprint, child number, chain code, 00 for private keys
 bip32_ext_key = b"\x04\x88\xad\xe4"  # xprv
 bip32_ext_key += b"\x00" * 9  # Depth, parent fingerprint, and child number
-bip32_ext_key += bip32_hash[32:]  # Chain code
-bip32_ext_key += b"\x00" + bip32_hash[:32]  # Master key
+bip32_ext_key += master_chain_code  # Chain code
+bip32_ext_key += b"\x00" + master_key  # Master key
 
 # Double hash using SHA256
 checksum = hashlib.sha256(bip32_ext_key).digest()
@@ -69,4 +75,29 @@ checksum = hashlib.sha256(checksum).digest()
 bip32_ext_key += checksum[:4]
 
 #print(bip32_ext_key.hex())
+print("bip32_ext_key: ") #or bip32 root key
 print(str(base58.b58encode(bip32_ext_key))[2:-1])
+
+
+#44' = bip44 = \x2c                        \x00 for privkey
+bip44_hash = hmac.new(master_chain_code, b'\x00' + master_key + b'\x80\x00\x00\x2c', digestmod=hashlib.sha512).digest()
+#depth = 01, fingerprint = ripemd(sha256(parentpriv)) or parentpubkey
+print(base58.b58decode("VnPvBVZi7cmfCdSH2GPKUhm14mpdXyq5bH").hex()[2:10])
+
+print("parent fingerprint: ")
+rip = hashlib.new('ripemd160')
+rip.update(hashlib.sha256(master_key).digest())
+print(rip.hexdigest())
+
+bip44_chain_code = bip44_hash[32:]
+bip44_key = bip44_hash[:32]
+bip44_key = bytes.fromhex(hex( ( int(bip44_key.hex(), 16) + int(master_key.hex(), 16) ) % N )[2:])
+print("bip44_key: ")
+print(bip44_key.hex())
+print("bip44_chain_code: ")
+print(bip44_chain_code.hex())
+
+vertcoin_seed = hmac.new(master_chain_code, b'\x00' + master_key + b'\x80\x00\x00\x1c', digestmod=hashlib.sha512).digest()
+vertcoin_chain_code = vertcoin_seed[32:]
+vertcoin_key = vertcoin_seed[:32]
+#vertcoin_key = ( vertcoin_key.hex() + master_key.hex() ) % N
